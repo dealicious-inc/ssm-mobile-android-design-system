782f854ce7f7610b0086adf47955c967ab55ed8a
강금실 <kanggumsil@deali.net>
2023-07-27 18:28:01 +0900

    [MP-3323] AnnotationString이 들어가는 DealiText 추가

--- designsystem/src/main/java/net/deali/designsystem/component/Text.kt
+++ designsystem/src/main/java/net/deali/designsystem/component/Text.kt
@@ -4,12 +4,37 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
 fun DealiText(
     text: String,
+    style: TextStyle,
+    color: Color,
+    modifier: Modifier = Modifier,
+    overflow: TextOverflow = TextOverflow.Clip,
+    maxLines: Int = Int.MAX_VALUE,
+) {
+    val mergedStyle = style.merge(
+        TextStyle(
+            color = color
+        )
+    )
+
+    BasicText(
+        text = text,
+        modifier = modifier,
+        overflow = overflow,
+        maxLines = maxLines,
+        style = mergedStyle
+    )
+}
+
+@Composable
+fun DealiText(
+    text: AnnotatedString,
     style: TextStyle,
     color: Color,
     modifier: Modifier = Modifier,
