b4a89987de8b7828ad77dc068d530ba00c1e26db
강금실 <kanggumsil@deali.net>
2023-08-03 14:42:57 +0900

    [MP-3356] Typography 폰트 원복

--- designsystem/src/main/java/net/deali/designsystem/theme/AppTypography.kt
+++ designsystem/src/main/java/net/deali/designsystem/theme/AppTypography.kt
@@ -246,7 +246,9 @@ fun defaultTypography(
 
 private val Pretendard = FontFamily(
     Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
     Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
 )
 
 internal val MaterialTypography = Typography(Pretendard)
