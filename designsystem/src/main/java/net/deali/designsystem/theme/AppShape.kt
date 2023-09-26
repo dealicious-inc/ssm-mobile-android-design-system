39acc8e46cbc14f2b5ac2a44fb3bed24e3c0a9d2
Yena Choi <yena@yena.io>
2023-09-26 10:55:16 +0900

    [MP-3546] radius 8 shape 추가

--- designsystem/src/main/java/net/deali/designsystem/theme/AppShape.kt
+++ designsystem/src/main/java/net/deali/designsystem/theme/AppShape.kt
@@ -15,6 +15,7 @@ data class AppShape(
     val radius100percent: Shape = RoundedCornerShape(percent = 100),
     val radius20: Shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
     val radius10: Shape = RoundedCornerShape(10.dp),
+    val radius8: Shape = RoundedCornerShape(8.dp),
 )
 
 internal val LocalShapes = staticCompositionLocalOf { AppShape() }
