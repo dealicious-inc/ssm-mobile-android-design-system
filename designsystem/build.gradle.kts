d825b2db40db92e3e17596578862eb1f2462b859
Jaewoong Cheon <cheonjaewoong@deali.net>
2023-12-12 17:46:39 +0900

    Compose Compiler Report 적용

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -7,6 +7,10 @@ plugins {
     id("kotlin-kapt")
 }
 
+// Compose를 사용하는 모듈에서 컴포저블과 클래스들의 안정성 상태 보고서 생성 활성화 옵션.
+// 필요할 때만 사용하기 위해 기본값은 false로 유지합니다.
+val isComposeCompilerReportsEnabled = false
+
 android {
     namespace = "net.deali.designsystem"
     compileSdk = libs.versions.targetSdk.get().toInt()
@@ -34,6 +38,18 @@ android {
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
+
+        if (isComposeCompilerReportsEnabled) {
+            freeCompilerArgs += listOf(
+                // 모듈에 있는 모든 컴포저블의 개수를 정리한 파일 생성.
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/report/compose-metrics",
+
+                // 모듈에 있는 컴포저블 함수와 매개변수, 그 외 여러 클래스들의 안정성 상태에 대한 정보 파일 생성.
+                "-P",
+                "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/report/compose-reports"
+            )
+        }
     }
 }
 
@@ -44,11 +60,12 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.foundation)
     implementation(libs.compose.tooling)
+    debugImplementation(libs.compose.preview)
     implementation(libs.compose.material)
+
     implementation(libs.android.material)
     implementation(libs.coil.compose)
     implementation(libs.snapper)
-    debugImplementation(libs.compose.preview)
 }
 
 afterEvaluate {
