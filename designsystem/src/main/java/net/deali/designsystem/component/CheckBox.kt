b8009253177bc53c36ab6e97b580aa4e9d2da14b
강해나 <kanggumsil@deali.net>
2024-02-14 10:55:50 +0900

    [MP-4217] checkbox에 clip 추가

--- designsystem/src/main/java/net/deali/designsystem/component/CheckBox.kt
+++ designsystem/src/main/java/net/deali/designsystem/component/CheckBox.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +31,7 @@ fun CheckBox(
 ) {
     Row(
         modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = rememberRipple(),
