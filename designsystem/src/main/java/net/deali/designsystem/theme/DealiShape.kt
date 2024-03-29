904efee25f57d8df514c4815a4f627c71ac57cd8
KimYonghyeok <dragon@deali.net>
2024-03-29 11:24:22 +0900

    [MP-4370] radius 16 추가

--- designsystem/src/main/java/net/deali/designsystem/theme/DealiShape.kt
+++ designsystem/src/main/java/net/deali/designsystem/theme/DealiShape.kt
@@ -12,8 +12,9 @@ object DealiShape {
     val radius3: Shape = RoundedCornerShape(3.dp)
     val radius4: Shape = RoundedCornerShape(4.dp)
     val radius6: Shape = RoundedCornerShape(6.dp)
-    val radius12: Shape = RoundedCornerShape(12.dp)
     val radius10: Shape = RoundedCornerShape(10.dp)
+    val radius12: Shape = RoundedCornerShape(12.dp)
+    val radius16: Shape = RoundedCornerShape(16.dp)
     val radius100: Shape = RoundedCornerShape(percent = 100)
     val radius20: Shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
 }
