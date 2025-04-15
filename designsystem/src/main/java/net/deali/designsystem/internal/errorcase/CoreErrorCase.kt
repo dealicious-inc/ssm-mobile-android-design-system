6108b0b6e616ad7fc9e75efbf90c0de9a9e10932
KimYonghyeok <dragon@deali.net>
2025-04-15 14:02:15 +0900

    [MP-5653] ErrorCase 배경색과 패딩을 받을 수 있게 추가

--- designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
+++ designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
@@ -1,31 +1,35 @@
 package net.deali.designsystem.internal.errorcase
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 internal fun CoreErrorCase(
+    backgroundColor: Color,
+    contentPadding: PaddingValues,
     modifier: Modifier = Modifier,
     content: @Composable ColumnScope.() -> Unit,
 ) {
-    val padding = ErrorCaseDefaults.padding
-
     Box(
         modifier = modifier
             .fillMaxSize()
+            .background(backgroundColor),
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.TopCenter)
-                .padding(padding),
+                .padding(contentPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
             content = content
         )
