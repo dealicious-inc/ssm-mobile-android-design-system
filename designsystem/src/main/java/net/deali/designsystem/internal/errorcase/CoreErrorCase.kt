55973aaa03bff3a9b2d193dc1f150afca0420513
KimYonghyeok <dragon@deali.net>
2025-04-16 17:31:30 +0900

    [MP-5653] ErrorCase fillMaxSize 제거

--- designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
+++ designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -22,7 +21,6 @@ internal fun CoreErrorCase(
 ) {
     Box(
         modifier = modifier
-            .fillMaxSize()
             .background(backgroundColor),
     ) {
         Column(
