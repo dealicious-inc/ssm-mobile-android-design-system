38fa76661b5d4cdc84b491fd7c4b440a4d21c6a8
KimYonghyeok <dragon@deali.net>
2025-04-17 14:33:06 +0900

    [MP-5653] 내부 padding을 modifier로 받게 수정

--- designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
+++ designsystem/src/main/java/net/deali/designsystem/internal/errorcase/CoreErrorCase.kt
@@ -2,6 +2,7 @@ package net.deali.designsystem.internal.errorcase
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,8 +12,11 @@ internal fun CoreErrorCase(
     modifier: Modifier = Modifier,
     content: @Composable ColumnScope.() -> Unit,
 ) {
+    val contentPadding = ErrorCaseDefaults.contentPadding
+
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .padding(contentPadding),
         horizontalAlignment = Alignment.CenterHorizontally,
         content = content
     )
