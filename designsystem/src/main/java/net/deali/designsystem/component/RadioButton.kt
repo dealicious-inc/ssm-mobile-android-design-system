74a0e3fd48f4510dfbd8dd331e93b944423b0465
soochun <soochun@deali.net>
2025-12-19 15:15:10 +0900

    [MP-6313] RadioButton 텍스트 없이 쓰는 부분 원복, RadioIcon 함수 open

--- designsystem/src/main/java/net/deali/designsystem/component/RadioButton.kt
+++ designsystem/src/main/java/net/deali/designsystem/component/RadioButton.kt
@@ -23,9 +23,9 @@ import net.deali.designsystem.util.getRandomText
 
 @Composable
 fun RadioButton(
+    text: String,
     selected: Boolean,
     modifier: Modifier = Modifier,
-    text: String = "",
     enabled: Boolean = true,
     onClick: () -> Unit
 ) {
@@ -47,25 +47,22 @@ fun RadioButton(
             selected = selected,
             enabled = enabled
         )
-
-        if (text.isNotEmpty()) {
-            DealiText(
-                modifier = Modifier
-                    .padding(start = 8.dp),
-                text = text,
-                style = DealiFont.b2r14,
-                color = when {
-                    enabled.not() -> DealiColor.g50
-                    else -> DealiColor.g100
-                }
-            )
-        }
+        DealiText(
+            modifier = Modifier
+                .padding(start = 8.dp),
+            text = text,
+            style = DealiFont.b2r14,
+            color = when {
+                enabled.not() -> DealiColor.g50
+                else -> DealiColor.g100
+            }
+        )
     }
 }
 
 
 @Composable
-private fun RadioIcon(
+fun RadioIcon(
     selected: Boolean,
     enabled: Boolean = true,
 ) {
@@ -101,22 +98,6 @@ private fun RadioButtonPreview() {
 
         RadioButton(
             text = getRandomText(3),
-            selected = false,
-            enabled = false,
-            onClick = {}
-        )
-
-        RadioButton(
-            selected = false,
-            onClick = {}
-        )
-
-        RadioButton(
-            selected = true,
-            onClick = {}
-        )
-
-        RadioButton(
             selected = false,
             enabled = false,
             onClick = {}
